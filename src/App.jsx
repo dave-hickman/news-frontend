70179c28d91e026f20c5300da0f92ac989269b14
dave-hickman <dave.robert.hickman@gmail.com>
2023-05-30 14:28:28 +0100

    install router dom and add browser router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import "./App.css";
 import Header from "./components/Header";
 import Articles from "./pages/Articles";
+import IndividualArticle from "./pages/IndividualArticle";
 
 function App() {
   const [userId, setUserId] = useState("davros");
@@ -12,6 +13,7 @@ function App() {
       <Nav userId={userId} />
       <Header />
       <Articles />
+      <IndividualArticle />
     </>
   );
 }
---
+++ src/pages/IndividualArticle.jsx
@@ -0,0 +1,5 @@
+const IndividualArticle = () => {
+    return <p>This is an individual article</p>
+}
+
+export default IndividualArticle
