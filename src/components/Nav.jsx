33dac37d8b1a12fa65ab19118c434c87c898b85b
dave-hickman <dave.robert.hickman@gmail.com>
2023-05-30 17:02:37 +0100

    style nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,13 @@
+import "../Nav.css"
+
 const Nav = ({ userId }) => {
   return (
     <nav>
+      <h1>NEWS!</h1>
+      <div className="links">
       <p>Home</p>
       <p>Topics</p>
+      </div>
       <p>User: {userId}</p>
     </nav>
   );
